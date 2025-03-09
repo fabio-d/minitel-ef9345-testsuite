b2c512f460c8f4cd8a209235caa6c10807276000
Fabio D'Urso <fabiodurso@hotmail.it>
2025-03-09 04:15:28 +0000

    Test resolutions that are only possible with the EF9345

--- tests/test_resolutions.py
+++ tests/test_resolutions.py
@@ -50,5 +50,45 @@ def test_80columns(video: VideoChip):
     assert screenshot.height == 2 + 25 * 10 + 2
 
 
+# 40 columns long mode, non-interlaced, 525 lines.
+@test(restrict=VideoChipType.EF9345)
+def test_40columns_525lines(video: VideoChip):
+    # Set TGS.
+    video.R1 = 0x11
+    video.ER0 = 0x81
+    video.wait_not_busy()
+
+    # Set PAT.
+    video.R1 = 0x00
+    video.ER0 = 0x83
+    video.wait_not_busy()
+
+    time.sleep(0.5)
+
+    screenshot = video.rgb_screenshot()
+    assert screenshot.width == 2 + 40 * 8 + 2
+    assert screenshot.height == 2 + 21 * 10 + 2
+
+
+# 80 columns long mode, non-interlaced, 525 lines.
+@test(restrict=VideoChipType.EF9345)
+def test_80columns_525lines(video: VideoChip):
+    # Set TGS.
+    video.R1 = 0xD1
+    video.ER0 = 0x81
+    video.wait_not_busy()
+
+    # Set PAT.
+    video.R1 = 0x00
+    video.ER0 = 0x83
+    video.wait_not_busy()
+
+    time.sleep(0.5)
+
+    screenshot = video.rgb_screenshot()
+    assert screenshot.width == 2 + 80 * 6 + 2
+    assert screenshot.height == 2 + 21 * 10 + 2
+
+
 if __name__ == "__main__":
     test_main()
