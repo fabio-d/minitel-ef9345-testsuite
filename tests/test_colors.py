65c1c492a4ebbc4d8ac6bdf7c78f41c52160dd40
Fabio D'Urso <fabiodurso@hotmail.it>
2025-04-26 08:05:08 +0000

    Test color attributes in 80 column mode

--- tests/test_colors.py
+++ tests/test_colors.py
@@ -2,7 +2,8 @@ import time
 from testlib import *
 
 # All the PAT bits that may affect how color and insert channels are rendered.
-ALL_PAT_CONFIGURATIONS = {  # pat_value -> description
+# Note: In 80 columns mode there is no conceal attribute.
+ALL_PAT_CONFIGURATIONS_40COLUMNS = {  # pat_value -> description
     0x00: "Conceal=0, Flash=0, InsMode=Inlay",
     0x10: "Conceal=0, Flash=0, InsMode=Boxing",
     0x20: "Conceal=0, Flash=0, InsMode=ChrMark",
@@ -20,11 +21,21 @@ ALL_PAT_CONFIGURATIONS = {  # pat_value -> description
     0x68: "Conceal=1, Flash=1, InsMode=ChrMark",
     0x78: "Conceal=1, Flash=1, InsMode=ActAreaMark",
 }
+ALL_PAT_CONFIGURATIONS_80COLUMNS = {  # pat_value -> description
+    0x00: "Flash=0, InsMode=Inlay",
+    0x10: "Flash=0, InsMode=Boxing",
+    0x20: "Flash=0, InsMode=ChrMark",
+    0x30: "Flash=0, InsMode=ActAreaMark",
+    0x40: "Flash=1, InsMode=Inlay",
+    0x50: "Flash=1, InsMode=Boxing",
+    0x60: "Flash=1, InsMode=ChrMark",
+    0x70: "Flash=1, InsMode=ActAreaMark",
+}
 
 # All the attributes that affect the output color and insert channels.
 # Note: The TS9347 has an extra attribute (i2 - stored in B), that is only
 # meaningful if the insert mode is "Boxing and Inlay" (see PAT) and i1 == 1.
-ALL_COLOR_ATTRIBUTES_EF9345 = {  # (a, b) -> description
+ALL_COLOR_ATTRIBUTES_EF9345_40COLUMNS = {  # (a, b) -> description
     (0x00, 0x00): "N=0, F=0, m=0, i=0",
     (0x00, 0x01): "N=0, F=0, m=0, i=1",
     (0x00, 0x04): "N=0, F=0, m=1, i=0",
@@ -42,7 +53,7 @@ ALL_COLOR_ATTRIBUTES_EF9345 = {  # (a, b) -> description
     (0x88, 0x04): "N=1, F=1, m=1, i=0",
     (0x88, 0x05): "N=1, F=1, m=1, i=1",
 }
-ALL_COLOR_ATTRIBUTES_TS9347 = {  # (a, b) -> description
+ALL_COLOR_ATTRIBUTES_TS9347_40COLUMNS = {  # (a, b) -> description
     (0x00, 0x00): "N=0, F=0, m=0, i1=0",
     (0x00, 0x01): "N=0, F=0, m=0, i1=1, i2=0",
     (0x00, 0x41): "N=0, F=0, m=0, i1=1, i2=1",
@@ -68,6 +79,16 @@ ALL_COLOR_ATTRIBUTES_TS9347 = {  # (a, b) -> description
     (0x88, 0x05): "N=1, F=1, m=1, i1=1, i2=0",
     (0x88, 0x45): "N=1, F=1, m=1, i1=1, i2=1",
 }
+ALL_COLOR_ATTRIBUTES_80COLUMNS = {  # a -> description
+    0x0: "N=0, F=0, D=0",
+    0x1: "N=0, F=0, D=1",
+    0x4: "N=0, F=1, D=0",
+    0x5: "N=0, F=1, D=1",
+    0x8: "N=1, F=0, D=0",
+    0x9: "N=1, F=0, D=1",
+    0xC: "N=1, F=1, D=0",
+    0xD: "N=1, F=1, D=1",
+}
 
 # All the MAT bits that may affect how the cursor is rendered.
 ALL_MAT_CONFIGURATIONS = {  # mat_value => description
@@ -80,7 +101,7 @@ ALL_MAT_CONFIGURATIONS = {  # mat_value => description
 
 
 def test_40columns_attributes_generator(video: VideoChip):
-    for pat_extra, pat_descr in ALL_PAT_CONFIGURATIONS.items():
+    for pat_extra, pat_descr in ALL_PAT_CONFIGURATIONS_40COLUMNS.items():
         yield f"pat{pat_extra:02x}", pat_extra, pat_descr
 
 
@@ -142,9 +163,9 @@ def test_40columns_attributes(video: VideoChip, pat_extra: int, pat_descr: str):
     # Draw color stripes with varying attributes.
     match video.chip_type:
         case VideoChipType.EF9345:
-            all_color_attributes = ALL_COLOR_ATTRIBUTES_EF9345
+            all_color_attributes = ALL_COLOR_ATTRIBUTES_EF9345_40COLUMNS
         case VideoChipType.TS9347:
-            all_color_attributes = ALL_COLOR_ATTRIBUTES_TS9347
+            all_color_attributes = ALL_COLOR_ATTRIBUTES_TS9347_40COLUMNS
     video.R0 = 0x01  # KRF/TLM with auto-increment.
     for row, ((a, b), description) in enumerate(
         all_color_attributes.items(),
@@ -185,11 +206,11 @@ def test_40columns_attributes(video: VideoChip, pat_extra: int, pat_descr: str):
 def test_40columns_cursor_generator(video: VideoChip):
     match video.chip_type:
         case VideoChipType.EF9345:
-            all_color_attributes = ALL_COLOR_ATTRIBUTES_EF9345
+            all_color_attributes = ALL_COLOR_ATTRIBUTES_EF9345_40COLUMNS
         case VideoChipType.TS9347:
-            all_color_attributes = ALL_COLOR_ATTRIBUTES_TS9347
+            all_color_attributes = ALL_COLOR_ATTRIBUTES_TS9347_40COLUMNS
 
-    for pat_extra, pat_descr in ALL_PAT_CONFIGURATIONS.items():
+    for pat_extra, pat_descr in ALL_PAT_CONFIGURATIONS_40COLUMNS.items():
         for mat_extra, mat_descr in ALL_MAT_CONFIGURATIONS.items():
             if mat_extra == 0:
                 # skip the "no cursor" test case, because it's already covered
@@ -296,6 +317,104 @@ def test_40columns_cursor(
     reference = Screenshot.load(
         "test_colors_data/test_40columns_cursor_%s_pat%02xmat%02xa%02xb%02x.png"
         % (video.chip_type.value, pat_extra, mat_extra, a, b)
+    )
+    if tgs_bgr == tgs_bir:
+        video.expect_screenshot(reference, ChannelSet.RGBI)
+    else:
+        video.R1 = tgs_bir
+        video.ER0 = 0x81
+        video.wait_not_busy()
+        video.expect_screenshot(reference, ChannelSet.RBI)
+        video.R1 = tgs_bgr
+        video.ER0 = 0x81
+        video.wait_not_busy()
+        video.expect_screenshot(reference, ChannelSet.RGB)
+
+
+def test_80columns_attributes_generator(video: VideoChip):
+    for pat_extra, pat_descr in ALL_PAT_CONFIGURATIONS_80COLUMNS.items():
+        yield f"pat{pat_extra:02x}", pat_extra, pat_descr
+
+
+@test(parametric=test_80columns_attributes_generator)
+def test_80columns_attributes(video: VideoChip, pat_extra: int, pat_descr: str):
+    # Set 40 columns long mode.
+    match video.chip_type:
+        case VideoChipType.EF9345:
+            tgs_bgr = 0xD0
+            tgs_bir = 0xD0
+            pat_base = 0x07
+        case VideoChipType.TS9347:
+            tgs_bgr = 0xC0
+            tgs_bir = 0xD0
+            pat_base = 0x03
+    video.R1 = tgs_bgr
+    video.ER0 = 0x81
+    video.wait_not_busy()
+    video.R1 = pat_base | pat_extra
+    video.ER0 = 0x83
+    video.wait_not_busy()
+
+    # mat
+    video.R1 = 0x02  # green
+    video.ER0 = 0x82
+    video.wait_not_busy()
+
+    # ror
+    video.R1 = 0x08
+    video.ER0 = 0x87
+    video.wait_not_busy()
+
+    # dor: c1 = red+insert, c0 = blue
+    match video.chip_type:
+        case VideoChipType.EF9345:
+            video.R1 = 0x94
+        case VideoChipType.TS9347:
+            # Note: On TS9347 DOR7 is used as Z4 (top bit of the current page),
+            # which we want to be set to 0. Furthermore, it seems that the D
+            # attribute acts itself as the insert bit, rather than selecting
+            # either i0 or i1 as the datasheet seems to say.
+            video.R1 = 0x14
+    video.ER0 = 0x84
+    video.wait_not_busy()
+
+    # Clear screen.
+    video.R1 = ord(" ")
+    video.R2 = ord(" ")
+    video.R3 = 0x00
+    video.R6 = 0  # y
+    video.R7 = 0  # x
+    video.ER0 = 0x05  # CLF/CLL
+    time.sleep(0.5)
+    video.ER0 = 0x91  # NOP to stop it.
+    video.wait_not_busy()
+
+    # Draw header.
+    video.R0 = 0x51  # KRL with auto-increment.
+    video.R3 = 0x11  # c1
+    video.R6 = 0  # y
+    video.R7 = 0  # x
+    for c in pat_descr:
+        video.ER1 = ord(c)
+        video.wait_not_busy()
+
+    # Draw color stripes with varying attributes.
+    for row, (a, description) in enumerate(
+        ALL_COLOR_ATTRIBUTES_80COLUMNS.items(),
+        start=8,
+    ):
+        video.R6 = row
+        video.R7 = 0x80  # column 1
+        video.R3 = a
+        video.ER1 = ord("@")
+        video.R7 = 0x81  # column 3
+        video.R3 = 0x11  # c1
+        for c in description:
+            video.ER1 = ord(c)
+            video.wait_not_busy()
+
+    reference = Screenshot.load(
+        "test_colors_data/test_80columns_attributes_pat%02x.png" % pat_extra
     )
     if tgs_bgr == tgs_bir:
         video.expect_screenshot(reference, ChannelSet.RGBI)
