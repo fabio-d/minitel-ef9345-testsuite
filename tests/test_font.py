f8ed7c512e8f1dd212a837fee48ac1c6b7ffae75
Fabio D'Urso <fabiodurso@hotmail.it>
2025-04-26 08:43:19 +0000

    Test underline cursor on non-alphanumeric characters

--- tests/test_font.py
+++ tests/test_font.py
@@ -159,6 +159,84 @@ def test_render_40columns(video: VideoChip, b: int):
     video.expect_screenshot(reference, ChannelSet.RGB)
 
 
+def test_cursor_underline_40columns_generator(video: VideoChip):
+    # The underline cursor should be visible in alphanumeric fonts...
+    yield "G0", 0x00, ord("A"), "alphanumeric"
+    yield "G0+underline", 0x00, ord("A"), "alphanumeric"  # same rendering as G0
+
+    # ...but not in semigraphical fonts.
+    yield "G10", 0x20, 0x66, "mosaic"
+    match video.chip_type:
+        case VideoChipType.EF9345:
+            yield "G11", 0x30, 0x0A, "EF9345_extra"
+        case VideoChipType.TS9347:
+            yield "GOE", 0x30, 0x0A, "TS9347_extra"
+
+
+@test(parametric=test_cursor_underline_40columns_generator)
+def test_cursor_underline_40columns(
+    video: VideoChip,
+    b: int,
+    c: int,
+    file_name: str,
+):
+    # Set 40 columns long mode.
+    match video.chip_type:
+        case VideoChipType.EF9345:
+            tgs = 0x10
+            pat = 0x37
+        case VideoChipType.TS9347:
+            tgs = 0x00
+            pat = 0x33
+    video.R1 = tgs
+    video.ER0 = 0x81
+    video.wait_not_busy()
+    video.R1 = pat
+    video.ER0 = 0x83
+    video.wait_not_busy()
+
+    # mat
+    video.R1 = 0x58  # fixed underlined cursor
+    video.ER0 = 0x82
+    video.wait_not_busy()
+
+    # ror
+    video.R1 = 0x08
+    video.ER0 = 0x87
+    video.wait_not_busy()
+
+    # dor
+    video.R1 = 0x00
+    video.ER0 = 0x84
+    video.wait_not_busy()
+
+    # Clear screen.
+    video.R1 = 0x00
+    video.R2 = 0x00
+    video.R3 = 0x00
+    video.R6 = 0  # y
+    video.R7 = 0  # x
+    video.ER0 = 0x05  # CLF/CLL
+    time.sleep(0.5)
+    video.ER0 = 0x91  # NOP to stop it.
+    video.wait_not_busy()
+
+    # Print the character being tested 3 times and leave the cursor on it.
+    video.R0 = 0x00  # KRF/TLM
+    video.R1 = c  # C
+    video.R2 = b  # B
+    video.R3 = 0x70  # A
+    video.R6 = 0  # y
+    for x in [0, 2, 4]:
+        video.ER7 = x
+        video.wait_not_busy()
+
+    reference = Screenshot.load(
+        "test_font_data/test_cursor_underline_40columns_%s.png" % file_name
+    )
+    video.expect_screenshot(reference, ChannelSet.RGB)
+
+
 @test()
 def test_render_80columns(video: VideoChip):
     # Set 80 columns long mode.
@@ -220,6 +298,84 @@ def test_render_80columns(video: VideoChip):
 
     reference = Screenshot.load(
         "test_font_data/test_render_80columns_%s.png" % video.chip_type.value
+    )
+    video.expect_screenshot(reference, ChannelSet.RGB)
+
+
+def test_cursor_underline_80columns_generator(video: VideoChip):
+    # The underline cursor should be visible.
+    yield "G0", 0x0, ord("A"), "alphanumeric"
+
+    # The underline cursor should *not* be visible if the U attribute is set.
+    yield "G0+underline", 0x2, ord("A"), "alphanumeric_underlined"
+
+    match video.chip_type:
+        case VideoChipType.EF9345:
+            yield "mosaic", 0x8, 0xE6, "EF9345_mosaic"
+        case VideoChipType.TS9347:
+            yield "extra", 0x0, 0x8A, "TS9347_extra"
+            yield "extra+underline", 0x2, 0x8A, "TS9347_extra_underlined"
+
+
+@test(parametric=test_cursor_underline_80columns_generator)
+def test_cursor_underline_80columns(
+    video: VideoChip,
+    a: int,
+    c: int,
+    file_name: str,
+):
+    # Set 80 columns long mode.
+    match video.chip_type:
+        case VideoChipType.EF9345:
+            tgs = 0xD0
+            pat = 0x37
+        case VideoChipType.TS9347:
+            tgs = 0xC0
+            pat = 0x33
+    video.R1 = tgs
+    video.ER0 = 0x81
+    video.wait_not_busy()
+    video.R1 = pat
+    video.ER0 = 0x83
+    video.wait_not_busy()
+
+    # mat
+    video.R1 = 0x58
+    video.ER0 = 0x82
+    video.wait_not_busy()
+
+    # ror
+    video.R1 = 0x08
+    video.ER0 = 0x87
+    video.wait_not_busy()
+
+    # dor
+    video.R1 = 0x07
+    video.ER0 = 0x84
+    video.wait_not_busy()
+
+    # Clear screen.
+    video.R1 = ord(" ")
+    video.R2 = ord(" ")
+    video.R3 = 0x00
+    video.R6 = 0  # y
+    video.R7 = 0  # x
+    video.ER0 = 0x05  # CLF/CLL
+    time.sleep(0.5)
+    video.ER0 = 0x91  # NOP to stop it.
+    video.wait_not_busy()
+
+    # Print the character being tested 3 times and leave the cursor on it.
+    video.R0 = 0x50  # KRL
+    video.R1 = c  # C
+    video.R3 = (a << 4) | a  # A
+    video.R6 = 0  # y
+    for x in [0, 1, 2]:
+        video.ER7 = x
+        video.wait_not_busy()
+
+    reference = Screenshot.load(
+        "test_font_data/test_cursor_underline_80columns_%s.png" % file_name
     )
     video.expect_screenshot(reference, ChannelSet.RGB)
 
